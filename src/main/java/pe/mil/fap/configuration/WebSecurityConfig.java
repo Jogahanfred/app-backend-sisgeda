fe778f13371466e8bf9534b3ae6437c29c337584
= <=>
2025-01-19 19:15:53 -0500

    Quinto commit

---
+++ src/main/java/pe/mil/fap/configuration/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package pe.mil.fap.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration 
+public class WebSecurityConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // Permite cualquier endpoint
+                        .allowedOrigins("*")  // Permite solicitudes desde cualquier origen
+                        .allowedMethods("*")  // Permite cualquier método HTTP
+                        .allowedHeaders("*"); // Permite cualquier encabezado
+            }
+        };
+    }
+}
